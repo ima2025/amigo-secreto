c3ddf412b5cacccdac02d7a7dd638ddc35e97459
ima2025 <imanolgonzalovelasco@gmail.com>
2025-01-27 12:33:56 -0300

    Funcion para actualizar lista amigos

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombres =[];
+    // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+    let amigos =[];
 
-function añadirAmigo () {
-    let nombreAmigos = document.getElementById("amigo").value;
+    function añadirAmigo () {
+        let nombreAmigos = document.getElementById("amigo").value;
 
-    if (nombreAmigos=="") {
-        alert("Por favor, inserte un nombre.");
-    }else{
-        //lo q esta en nombreAmigos será introducido dentro del array nombres
-        nombres.push(nombreAmigos);
-        limpiarCaja();
+        if (nombreAmigos=="") {
+            alert("Por favor, inserte un nombre.");
+        }else{
+            amigos.push(nombreAmigos);
+            limpiarCaja();
+        }
     }
-}
-function limpiarCaja(){
-    document.getElementById("amigo").value = "";
-}
+
+    function limpiarCaja(){
+        document.getElementById("amigo").value = "";
+    }
+
+
+    function recorreListaAmigos() {
+        // Obtener lista <ul>
+        let lista = document.getElementById("listaAmigos");
+        // Limpiar el contenido actual de la lista para evitar duplicados
+        lista.innerHTML = "";
+        // Crear una cadena que contendrá todo el contenido de la lista
+        let contenido = "";
+        //se recorre la lista
+        for (let i = 0; i < amigos.length; i++) {
+            contenido = contenido + "<li>" + amigos[i] + "</li>";
+        }
+    // Asignar el contenido generado al innerHTML de la lista (fuera del bucle)
+    lista.innerHTML = contenido;
+    }
